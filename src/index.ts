08a755abb513d29466f05ed08793d1ecf7807b53
Martin Kapal <flamecze@gmail.com>
2025-03-31 00:32:14 +0200

    Better type safety of codepages, extract control character from codepages & specials

--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,56 @@
-const codepages: Record<string, string> = {
-  "^L": "CP1252",
-  "^G": "CP1253",
-  "^C": "CP1251",
-  "^E": "CP1250",
-  "^T": "CP1254",
-  "^B": "CP1257",
-  "^J": "shift-jis",
-  "^H": "big5",
-  "^S": "gbk",
-  "^K": "euc-kr",
-  "^8": "CP1252",
-};
+const controlChar = "^";
+
+const codepages = {
+  "8": "CP1252",
+  L: "CP1252",
+  G: "CP1253",
+  C: "CP1251",
+  E: "CP1250",
+  T: "CP1254",
+  B: "CP1257",
+  J: "shift-jis",
+  H: "big5",
+  S: "gbk",
+  K: "euc-kr",
+} as const;
+
+type Codepage = keyof typeof codepages;
 
 const specials: Record<string, string> = {
-  "^v": "|",
-  "^a": "*",
-  "^c": ":",
-  "^d": "\\",
-  "^s": "/",
-  "^q": "?",
-  "^t": '"',
-  "^l": "<",
-  "^r": ">",
-  "^h": "#",
+  v: "|",
+  a: "*",
+  c: ":",
+  d: "\\",
+  s: "/",
+  q: "?",
+  t: '"',
+  l: "<",
+  r: ">",
+  h: "#",
+  "^": "^^",
 };
 
-const isMultiByte = (cp: string, c: number): boolean => {
-  switch (cp) {
-    case "^L":
-    case "^8":
-    case "^G":
-    case "^C":
-    case "^E":
-    case "^T":
-    case "^B":
+const isMultiByte = (codepage: Codepage, character: number): boolean => {
+  switch (codepage) {
+    case "L":
+    case "8":
+    case "G":
+    case "C":
+    case "E":
+    case "T":
+    case "B":
       return false;
-    case "^J":
-      return (c > 0x80 && c < 0xa0) || (c >= 0xe0 && c < 0xfd);
-    case "^H":
-    case "^S":
-    case "^K":
-      return c > 0x80 && c < 0xff;
+    case "J":
+      return (
+        (character > 0x80 && character < 0xa0) ||
+        (character >= 0xe0 && character < 0xfd)
+      );
+    case "H":
+    case "S":
+    case "K":
+      return character > 0x80 && character < 0xff;
     default:
-      throw new Error(`Unknown Codepage: ${c}`);
+      throw new Error(`Unknown Codepage: ${character}`);
   }
 };
 
@@ -53,7 +61,7 @@ function parseLFSMessage(msg: Uint8Array | string): string {
       : msg;
 
   // Default codepage: Latin 1
-  let currentCodepage = "^L";
+  let currentCodepage: Codepage = "L";
   let resultString = "";
   let blockStart = 0;
   let blockEnd = 0;
@@ -71,10 +79,10 @@ function parseLFSMessage(msg: Uint8Array | string): string {
       // Skip multi-byte char
       blockEnd += 2;
       i++;
-    } else if (buffer[i] === 0x5e) {
+    } else if (buffer[i] === controlChar.charCodeAt(0)) {
       // Found '^'
-      let cpCheck = `^${iconvCurrent.decode(buffer.slice(i + 1, i + 2))}`;
-      if (codepages.hasOwnProperty(cpCheck)) {
+      let cpCheck = iconvCurrent.decode(buffer.slice(i + 1, i + 2));
+      if (isValidCodepage(cpCheck)) {
         if (blockStart < blockEnd) {
           // Convert current block if it has data
           resultString += iconvCurrent.decode(
@@ -93,6 +101,18 @@ function parseLFSMessage(msg: Uint8Array | string): string {
         }
         blockEnd = i + 2;
         i++;
+      } else if (specials.hasOwnProperty(cpCheck)) {
+        resultString += specials[cpCheck];
+
+        // Start a new block
+        if (buffer[i + 1] === 0x38) {
+          blockStart = i;
+        } else {
+          blockStart = i + 2;
+        }
+        blockEnd = i + 2;
+
+        i++;
       } else {
         // Skip escaped/colour change character
         blockEnd += 2;
@@ -104,12 +124,11 @@ function parseLFSMessage(msg: Uint8Array | string): string {
     }
   }
 
-  for (let special in specials) {
-    const regExp = new RegExp(`(?<!\\^)\\${special}`, "g");
-    resultString = resultString.split(regExp).join(specials[special]);
-  }
-
   return resultString;
+}
+
+function isValidCodepage(codepage: string): codepage is Codepage {
+  return codepages.hasOwnProperty(codepage);
 }
 
 export default parseLFSMessage;
